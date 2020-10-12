bbec4a164ffc0af1b2cf11ebfabde0442fd15b29
yermakova-l <lyudmyla.yermakova@gmail.com>
2020-10-12 17:38:43 +0300

    Added @CrossOrigin(origins = *) to UserController to solve react CORS problem

--- hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserController.java
+++ hoaxify/src/main/java/com/hoaxify/hoaxify/user/UserController.java
@@ -3,11 +3,13 @@ package com.hoaxify.hoaxify.user;
 import com.hoaxify.hoaxify.shared.GenericResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
